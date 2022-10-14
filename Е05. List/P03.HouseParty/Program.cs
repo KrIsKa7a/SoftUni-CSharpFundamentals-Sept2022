548d0296091356587c43af5fb6555f5b4268ca97
KrIsKa7a <kristiqn.ivanov@abv.bg>
2022-10-14 21:59:54 +0300

    Upload solutions for E05. List

---
+++ Е05. List/P03.HouseParty/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guestList = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = cmdArgs[0];
+
+                if (cmdArgs.Length == 3)
+                {
+                    //is going
+                    if (guestList.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                        continue;
+                    }
+
+                    guestList.Add(name);
+                }
+                else if (cmdArgs.Length == 4)
+                {
+                    //is not going
+                    if (!guestList.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                        continue;
+                    }
+
+                    guestList.Remove(name);
+                }
+            }
+
+            PrintGuestList(guestList);
+        }
+
+        static void PrintGuestList(List<string> guestList)
+        {
+            //Change in the list here will be appended in the Main()
+            foreach (string name in guestList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        static void PrintGuestListAlphabetically(List<string> guestList)
+        {
+            //OrderByDescending = OrderBy + Reverse
+            List<string> orderedList = guestList
+                .OrderBy(e => e) //Will learn it later in the course!
+                .ToList();
+            foreach (string name in orderedList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Е05. List/P08.AnonymousThreat/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P08.AnonymousThreat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "3:1")
+            {
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string cmdType = cmdArgs[0];
+
+                if (cmdType == "merge")
+                {
+                    int startIndex = int.Parse(cmdArgs[1]);
+                    int endIndex = int.Parse(cmdArgs[2]);
+
+                    FixInvalidIndexes(words, ref startIndex, ref endIndex);
+                    MergeWords(words, startIndex, endIndex);
+                }
+                else if (cmdType == "divide")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    int partitions = int.Parse(cmdArgs[2]);
+
+                    //abcde -> ab, bc, e (Wrong, we want the last one to be the longest)
+                    //abcde -> a, b, cde 5 : 3 = 1
+                    //abcdef -> ab, cd, ef
+                    string word = words[index];
+                    List<string> partitionsList = DivideWord(word, partitions);
+
+                    words.RemoveAt(index);
+                    words.InsertRange(index, partitionsList);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", words));
+        }
+
+        static void FixInvalidIndexes(List<string> words, ref int startIndex, ref int endIndex)
+        {
+            if (startIndex < 0)
+            {
+                //First possible
+                startIndex = 0;
+            }
+
+            if (startIndex >= words.Count)
+            {
+                startIndex = words.Count - 1;
+            }
+
+            if (endIndex <= 0)
+            {
+                endIndex = 0;
+            }
+
+            if (endIndex >= words.Count)
+            {
+                //Last possible
+                endIndex = words.Count - 1;
+            }
+        }
+
+        static void MergeWords(List<string> words, int startIndex, int endIndex)
+        {
+            string mergedText = string.Empty;
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                string currWord = words[startIndex];
+                mergedText += currWord;
+                words.RemoveAt(startIndex);
+            }
+
+            words.Insert(startIndex, mergedText);
+        }
+
+        static List<string> DivideWord(string word, int partitions)
+        {
+            int substringsLength = word.Length / partitions;
+            int lastSubstringLength = substringsLength + word.Length % partitions;
+            //int lastSubstringLength = word.Length - ((partitions - 1) * substringsLength);
+
+            List<string> partitionsList = new List<string>();
+            //All without the last one
+            for (int i = 0; i < partitions; i++)
+            {
+                int desiredLength = substringsLength;
+                if (i == partitions - 1)
+                {
+                    desiredLength = lastSubstringLength;
+                }
+
+                char[] newPartitionArr = word
+                    .Skip(i * substringsLength)
+                    .Take(desiredLength)
+                    .ToArray();
+                string newPartition = String.Join("", newPartitionArr);
+                partitionsList.Add(newPartition);
+            }
+
+            return partitionsList;
+        }
+    }
+}
